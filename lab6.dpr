a41c8e289beb0ee3f3df0a030607e46a2bfa33c6
Alexandr Pankratiew <sasha.pankratiew@gmail.com>
2017-11-21 16:51:10 +0300

    Global Update: Fixed more bugs of algorithm and add new functions: - Assigning an arbitrary location of points and roads; - Now the condition is taken into account that robots can not travel on the same road; - And more other

--- lab6.dpr
+++ lab6.dpr
@@ -1,8 +1,10 @@
-program lab6_mb_lu4we;
+program lab6;
 
 {$APPTYPE CONSOLE}
 uses
-  SysUtils, Windows;
+  SysUtils,
+  Windows;
+
 const
   y=50;
   x=50;
@@ -13,31 +15,20 @@ const
   MaxSpeed2 = 2;
 type
   TRobotsCoords = array[1..3, 1..MaxRobots] of integer;
-  //TDictanceMatrix = array[1..MaxRobots, 1..MaxRobots] of real;
   TPointMatrix = array [1..x, 1..(y*MaxRobots)] of string;
+  TCityMatrix = array [1..x, 1..y] of byte;
   TError = string;
-  // DELETE BEGIN
-  // TTempPointMatrix = array[1..x,1..y] of string;
-  // DELETE END
 var ix,jy: byte;
   NRob: byte;
   RobotsInfo: TRobotsCoords;
-  //DictanceMatrix: TDictanceMatrix;
-  i,j,kn, finalTime:Byte;
+  i,j,kn,b, finalTime:Byte;
   err:TError;
   PointMatrix: TPointMatrix;
-
-procedure clrscr;
-var
-  cursor: COORD;
-  r: cardinal;
-begin
-  r := 300;
-  cursor.X := 0;
-  cursor.Y := 0;
-  FillConsoleOutputCharacter(GetStdHandle(STD_OUTPUT_HANDLE), ' ', 80 * r, cursor, r);
-  SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), cursor);
-end;
+  CityMatrix: TCityMatrix;
+  cnt:Byte;
+  starttime:byte;
+  charfill: char;
+  finalx, finaly: Byte;
 
 Function IntToStr(I : Longint) : String;
 Var S : String;
@@ -63,10 +54,36 @@ begin
   end;
 end;
 
+function isElementRepeated(arr:TPointMatrix; el: string; line: Integer):boolean;
+var i:Integer;
+brk:Boolean;
+Repeated:Boolean;
+tmp:string;
+begin
+  i:=1;
+  brk:=false;
+  Repeated:=false;
+  while ((i <MaxX*MaxRobots) and (not brk)) do
+  begin
+    if (arr[line,i] = el) then
+    begin
+      brk:=True;
+      Repeated:=True;
+    end;
+    tmp:=el[4]+el[5]+el[3]+el[1]+el[2];
+    if (arr[line,i] = tmp) then
+    begin
+      brk:=True;
+      Repeated:=True;
+    end;
+    inc(i);
+  end;
+  isElementRepeated:=Repeated;
+end;
+
 procedure searchRelatedPoint(robot,speed, x, y, step: integer);
 var rob, time,cf: integer;
 currel: string;
-//var PointMatrix: TTempPointMatrix;
 begin
   cf:= 1 + (robot-1)*MaxX;
   rob:=robot;
@@ -74,50 +91,79 @@ begin
     1 : time:=2;
     2 : time:=1;
   end;
-  if (step < 10) then
+  if (step < 12) then
   begin
-    if ((x-1) > 0) then
+    if ( ((x-2) > 0) and (CityMatrix[y,x-2] <> 0) and (CityMatrix[y,x-1] = 2)) then
     begin
-      currel := IntToStr(x-1) + IntToStr(y);
-      push(PointMatrix,cf,currel,step);
-      searchRelatedPoint(rob,speed, x-1,y, step+time);
+      currel := IntToStr(x) + IntToStr(y) + '>' + IntToStr(x-2) + IntToStr(y);
+      if(not isElementRepeated(PointMatrix, currel, step)) then
+        push(PointMatrix,cf,currel,step);
+      searchRelatedPoint(rob,speed, x-2,y, step+time);
     end;
-    if ((x+1) < ix) then
+    if ( ((x+2) <= ix) and (CityMatrix[y,x+2] <> 0) and (CityMatrix[y,x+1] = 2)) then
     begin
-      currel := IntToStr(x+1) + IntToStr(y);
-      push(PointMatrix,cf,currel,step);
-      searchRelatedPoint(rob,speed, x+1,y, step+time);
+      currel := IntToStr(x) + IntToStr(y) + '>' + IntToStr(x+2) + IntToStr(y);
+      if(not isElementRepeated(PointMatrix, currel, step)) then
+        push(PointMatrix,cf,currel,step);
+      searchRelatedPoint(rob,speed, x+2,y, step+time);
     end;
-    if ((y-1) > 0) then
+    if ( ((y-2) > 0) and (CityMatrix[y-2,x] <> 0) and (CityMatrix[y-1,x] = 2)) then
     begin
-      currel := IntToStr(x) + IntToStr(y-1);
-      push(PointMatrix,cf,currel,step);
-      searchRelatedPoint(rob,speed, x,y-1, step+time);
+      currel := IntToStr(x) + IntToStr(y) + '>' + IntToStr(x) + IntToStr(y-2);
+      if(not isElementRepeated(PointMatrix, currel, step)) then
+        push(PointMatrix,cf,currel,step);
+      searchRelatedPoint(rob,speed, x,y-2, step+time);
     end;
-    if ((y+1) < jy) then
+    if (((y+2) <= jy) and (CityMatrix[y+2,x] <> 0) and (CityMatrix[y+1,x] = 2)) then
     begin
-      currel := IntToStr(x) + IntToStr(y+1);
-      push(PointMatrix,cf,currel,step);
-      searchRelatedPoint(rob,speed, x,y+1, step+time);
+      currel := IntToStr(x) + IntToStr(y) + '>' + IntToStr(x) + IntToStr(y+2);
+      if(not isElementRepeated(PointMatrix, currel, step)) then
+        push(PointMatrix,cf,currel,step);
+      searchRelatedPoint(rob,speed, x,y+2, step+time);
     end;
   end;
 end;
 
 function searchSameElements(var arr:TPointMatrix; el: string; line:Integer):integer;
 var j,time, count: Integer;
+prevs, currs, els,elneed,currneed:string;
 brk:boolean;
 begin
   brk:=false;
-  count:=0;
+  count:=1;
   if (el <> '') then
   begin
-    j:=1;
+    j:=MaxY+1;
+    prevs:=el[1] + el[2];
     while((j<MaxY*MaxRobots) and (not brk)) do
     begin
-      if (arr[line,j] = el) then
+      if (arr[line, j] <> '') then
       begin
-        Inc(count);
-        j:= count*MaxY+1{(j mod MaxY) + MaxY};
+        currs :=  arr[line,j][1] + arr[line,j][2];
+        els := el[1] + el[2];
+        currneed:= arr[line,j][4] + arr[line,j][5];
+        elneed:= el[4] + el[5];
+        //write(' - ', count:4);
+        {Writeln(currs);
+        writeln;
+        Writeln(currs);
+        Writeln(Prevs);   }
+        if ((currneed = elneed)
+            and (currs <> prevs)
+            and (currs <> els))
+        then
+        begin
+          //Writeln('curr', currs);
+          //Writeln('prev', prevs);
+          //Writeln('els', els);
+          //writeln(currneed);
+          //writeln(elneed);
+          //writeln(line);
+          //writeln(count);
+          Inc(count);
+          prevs:=currs;
+          j:= count*MaxY{(j mod MaxY) + MaxY};
+        end;
       end;
       inc(j);
     end;
@@ -146,6 +192,8 @@ begin
         begin
           brk:=true;
           time:=i;
+          finalx:=StrToInt(PointMatrix[i,j][5]);
+          finaly:=StrToInt(PointMatrix[i,j][4]);
           {Writeln(j,' ',i);
           Writeln(arr[i,j]);}
         end;
@@ -159,95 +207,120 @@ begin
   getTime:= time;
 end;
 
+procedure createCityVisualisation(var arr:TCityMatrix; finalmod:boolean=false);
+var i,j:Byte;
 begin
-  //ix:=10;
-  //jy:=10;
+  Writeln;
+  write('+');
+  for i:=1 to (ix*3+2) do
+    write('-');
+  Writeln('+');
+  for i:= 1 to ix do
+  begin
+    write('|');
+    for j:=1 to jy do
+    begin
+      cnt:=0;
+      if (finalmod) then
+      begin
+        if ((i = finalx) and (j = finaly)) then
+        begin
+          inc(cnt);
+          write(' ', NRob,'R');
+        end;
+      end
+      else
+      begin
+        for b:=1 to NRob do
+        begin
+          if (RobotsInfo[1,b] = j) and (RobotsInfo[2,b] = i) then
+          begin
+            write('R':3);
+            inc(cnt);
+          end;
+        end;
+      end;
+      if ((arr[i,j] = 1) and (cnt = 0)) then
+      begin
+        inc(cnt);
+        write('D':3);
+      end;
+      if ((arr[i,j] = 2) and (cnt = 0)) then
+      begin
+        inc(cnt);
+        write('=':3);
+      end;
+      if (cnt = 0) then
+        write(' ':3);
+    end;
+    writeln('  |');
+  end;
+  write('+');
+  for i:=1 to (ix*3+2) do
+    write('-');
+  Writeln('+');
+  Writeln;
+end;
 
-  {searchRelatedPoint(2,2,3,3,1,1);
-  searchRelatedPoint(3,1,5,5,1,1);  }
-  // Writeln(PointMatrix[1,44] <> '');
-  //PointMatrix[1,51] := '1';
-  //writeln(PointMatrix[1,51]);
-  {for i :=1 to 22 do
+procedure writeVisitedPointsMatrix(arr:TPointMatrix);
+begin
+  for i :=1 to 12 do
   begin
     for j :=1 to 50 do
-        write ( PointMatrix[i,j], ' ');
+        write (arr[i,j], ' ');
    writeln ;
   end;
   Writeln;
-  for i :=1 to 22 do
+  for i :=1 to 12 do
   begin
     for j :=51 to 100 do
-        write ( PointMatrix[i,j], ' ');
+        write ( arr[i,j], ' ');
    writeln ;
   end;
-  Writeln;
-  for i :=1 to 22 do
+  for i :=1 to 12 do
   begin
     for j :=101 to 150 do
-        write ( PointMatrix[i,j], ' ');
+        write ( arr[i,j], ' ');
    writeln ;
-  end;    }
-  //Writeln(getTime(PointMatrix,3)*2);
-
- { ix:= 5;
-  jy:=5;
-  NRob:= 2;
-  RobotsInfo[1,1] := 1;
-  RobotsInfo[2,1] := 1;
-  RobotsInfo[3,1] := 1;
-  RobotsInfo[1,2] := 2;
-  RobotsInfo[2,2] := 2;
-  RobotsInfo[3,2] := 1;
-  }
-  repeat
-    Writeln('Enter the number of points in width (max: 50)');
-    Readln(ix);
-    if (ix > 50) then
-      Writeln('The maximum value is 50. Please enter again');
-  until(ix <= 50);
-  repeat
-    Writeln('Enter the number of points in height (max: ',50 div ix,')');
-    Readln(jy);
-    if (ix > 50 div ix) then
-      Writeln('The maximum value is ',50 div ix,'. Please enter again');
-  until(jy <= (50 div ix));
-  repeat
-    writeln('Enter the number of robots');
-    readln(NRob);
-    if (NRob > MaxRobots) then
-      Writeln('Max. number of robots: ', MaxRobots);
-  until (NRob <= MaxRobots);
-  for i:=1 to NRob do
-  begin
-    repeat
-      err := '';
-      Write('Enter the initial X coordinate of the robot number ', i, ': ');
-      Readln(RobotsInfo[1,i]);
-      if(RobotsInfo[1,i] > ix) then
-        err:= err + #10 + '- You are outside the X coordinate';
-      Write('Enter the initial Y coordinate of the robot number ', i, ': ');
-      Readln(RobotsInfo[2,i]);
-      if(RobotsInfo[2,i] > jy) then
-        err:= err + #10 + '- You are outside the Y coordinate';
-      Write('Enter robot speed number ', i, '(speed = ',MaxSpeed1,' or ',MaxSpeed2,')', ': ');
-      Readln(RobotsInfo[3,i]);
-      if((RobotsInfo[3,i] > 2) or (RobotsInfo[3,i] < 1)) then
-        err:= err + #10 + '- The speed can take two values: 1 or 2';
-      if (err <> '') then
-        writeln(#10,'=======================================',
-                #10,'Errors: ', err,#10,
-                'Please enter data for this robot again',
-                #10,'=======================================');
-
-    until((RobotsInfo[1,i] <= ix)
-          and
-         (RobotsInfo[2,i] <= jy)
-          and
-         ((RobotsInfo[3,i] = MaxSpeed1) or (RobotsInfo[3,i] = MaxSpeed2)));
   end;
-  clrscr;
-  Writeln('The city contains points [',ix,',',jy,']');
+end;
+
+begin
+  // CITY AND ROBOTS INFORMATION BEGIN
+  ix:=3*2-1; //  Width and length
+  jy:=3*2-1; //  of the city
+
+  CityMatrix[1,1]:=1;  // 1 - Point
+  CityMatrix[2,1]:=2;  // 2 - Road
+  CityMatrix[3,1]:=1;
+  CityMatrix[3,2]:=2;
+  CityMatrix[3,3]:=1;
+  CityMatrix[3,4]:=2;
+  CityMatrix[3,5]:=1;
+  CityMatrix[2,5]:=2;
+  CityMatrix[1,5]:=1;
+  CityMatrix[4,3]:=2;
+  CityMatrix[5,1]:=1;
+  CityMatrix[5,2]:=2;
+  CityMatrix[5,3]:=1;
+  CityMatrix[5,4]:=2;
+  CityMatrix[5,5]:=1;
+
+  NRob:= 3; // Number of robots, max - 3;
+  RobotsInfo[1,1] := 1; // Robot's coord X
+  RobotsInfo[2,1] := 1; // Robot's coord Y
+  RobotsInfo[3,1] := 1; // Robot's speed (1/2)
+  RobotsInfo[1,2] := 5;
+  RobotsInfo[2,2] := 5;
+  RobotsInfo[3,2] := 1;
+  RobotsInfo[1,3] := 1;
+  RobotsInfo[2,3] := 5;
+  RobotsInfo[3,3] := 2;
+  // CITY AND ROBOTS INFORMATION END
+
+  createCityVisualisation(CityMatrix);
+
+  Writeln('The city contains points [',(ix div 2)+1,',',jy div 2+1,']');
   Writeln(NRob, ' robots drive around the city');
   Writeln('+-------+---------+---------+---------+');
   Writeln('| Robot | Initial | Initial |  Robot  |');
@@ -261,36 +334,25 @@ begin
 
   for kn:=1 to NRob do
   begin
-    searchRelatedPoint(kn,RobotsInfo[3,kn],RobotsInfo[1,kn],RobotsInfo[2,kn],1);
+    Case RobotsInfo[3,kn] of
+      1 : starttime:=2;
+      2 : starttime:=1;
+    end;
+    searchRelatedPoint(kn,RobotsInfo[3,kn],RobotsInfo[1,kn],RobotsInfo[2,kn],starttime);
   end;
-  {
-  for i :=1 to 22 do
-  begin
-    for j :=1 to 50 do
-        write ( PointMatrix[i,j], ' ');
-   writeln ;
-  end;
+
+  // writeVisitedPointsMatrix(PointMatrix);
+
   Writeln;
-  for i :=1 to 22 do
-  begin
-    for j :=51 to 100 do
-        write ( PointMatrix[i,j], ' ');
-   writeln ;
-  end;
-  Writeln;
-  for i :=1 to 22 do
-  begin
-    for j :=101 to 150 do
-        write ( PointMatrix[i,j], ' ');
-   writeln ;
-  end;
-  }
   finalTime := getTime(PointMatrix,NRob)*5;
   if  (finalTime = 0) then
     Writeln('Robots will never meet :c')
   else
-    Writeln('Robots met through ',(finalTime/10):0:1, 'T');
+    Writeln('Hooray! Robots met later ',(finalTime/10):0:1, 'T');
 
+  createCityVisualisation(CityMatrix, true);
+
+  Writeln(#10, 'Press any key to exit');
   Readln;
 
 end.
